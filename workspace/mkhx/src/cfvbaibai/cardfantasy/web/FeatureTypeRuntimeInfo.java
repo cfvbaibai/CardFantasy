ddcfcb0e15f941ff4360cba06a37809de6bf9b31
cfvbaibai <tonywhitewhite@hotmail.com>
2013-10-19 23:27:36 +0800

    Submit missing new file

---
+++ workspace/mkhx/src/cfvbaibai/cardfantasy/web/FeatureTypeRuntimeInfo.java
@@ -0,0 +1,21 @@
+package cfvbaibai.cardfantasy.web;
+
+import cfvbaibai.cardfantasy.data.FeatureType;
+
+public class FeatureTypeRuntimeInfo {
+
+    public FeatureTypeRuntimeInfo(FeatureType featureType) {
+        this.name = featureType.name();
+        this.growable = featureType.isGrowable();
+    }
+
+    private String name;
+    private boolean growable;
+    
+    public String getName() {
+        return name;
+    }
+    public boolean isGrowable() {
+        return growable;
+    }
+}
