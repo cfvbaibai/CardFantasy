e614e78c072d0e04f399f27f19f6b73dccd6c1ca
cfvbaibai <cfvbaibai@163.com>
2014-12-16 22:08:53 +0800

    Add test case for Racial Attack vs. Weaken.

--- workspace/CardFantasyCore/src/cfvbaibai/cardfantasy/test/RacialBufferTest.java
+++ workspace/CardFantasyCore/src/cfvbaibai/cardfantasy/test/RacialBufferTest.java
@@ -32,14 +32,13 @@ public class RacialBufferTest {
     
     @Test
     public void test种族之力_削弱() {
-        random.addNextPicks(0).addNextNumbers(0); // 保证独眼巨人摧毁隐世先知
-
         FeatureTestContext context = FeatureValidationTests.prepare(50, 50, "隐世先知-5", "大剑圣", "独眼巨人");
         context.addToHand(0, 0).setSummonDelay(0);
         CardInfo c大剑圣 = context.getPlayer(0).getHand().addCard(context.getCardInfo(1, 0)).setSummonDelay(0);
         context.addToHand(2, 1).setSummonDelay(0);
 
         context.getStage().gameStarted();
+        random.addNextPicks(0).addNextNumbers(0); // 保证独眼巨人摧毁隐世先知
         context.getEngine().proceedOneRound();
         context.getEngine().proceedOneRound();
 
@@ -75,13 +74,13 @@ public class RacialBufferTest {
 
     @Test
     public void test种族之力_种族克制() {
-        random.addNextNumbers(1000); // 猎魔犬不暴击
         FeatureTestContext context = FeatureValidationTests.prepare(50, 50, "猎魔犬", "隐世先知", "骨龙");
         context.addToField(0, 0);
         context.addToHand(1, 0).setSummonDelay(0);
         CardInfo c骨龙 = context.addToField(2, 1);
 
         context.getStage().gameStarted();
+        random.addNextNumbers(1000); // 猎魔犬不暴击
         context.getEngine().proceedOneRound();
 
         Assert.assertEquals((int)((280 + 175 /* 王国之力 */) * 1.6 /* 圣光 */), 1250 - c骨龙.getHP());
@@ -89,14 +88,25 @@ public class RacialBufferTest {
     
     @Test
     public void test种族克制_暴击() {
-        random.addNextNumbers(0); // 猎魔犬暴击
         FeatureTestContext context = FeatureValidationTests.prepare(50, 50, "猎魔犬", "骨龙");
         context.addToField(0, 0);
         CardInfo c骨龙 = context.addToField(1, 1);
 
         context.getStage().gameStarted();
+        random.addNextNumbers(0); // 猎魔犬暴击
         context.getEngine().proceedOneRound();
 
         Assert.assertEquals((280 * 100 + 280 * 60 /* 暴击 */ + 280 * 60 /* 圣光 */) / 100, 1250 - c骨龙.getHP());
+    }
+    
+    @Test
+    public void test种族克制_削弱() {
+        FeatureTestContext context = FeatureValidationTests.prepare(50, 50, "东方僧人-1", "穴居人奴隶-5");
+        CardInfo c东方僧人 = context.addToField(0, 0);
+        context.addToField(1, 1);
+        context.getStage().gameStarted();
+        context.getEngine().proceedOneRound();
+        context.getEngine().proceedOneRound();
+        Assert.assertEquals((180 - 30 /* 削弱 */) * 175 / 100 /* 暗杀 */, 620 - c东方僧人.getHP());
     }
 }
