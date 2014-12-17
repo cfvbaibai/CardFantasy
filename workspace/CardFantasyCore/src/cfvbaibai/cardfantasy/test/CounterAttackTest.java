8a1d2057b9fb7798c1d6404de0495d132b2a550b
cfvbaibai <cfvbaibai@163.com>
2014-12-17 23:40:11 +0800

    Add test横扫_雷盾 and test横扫_反击

--- workspace/CardFantasyCore/src/cfvbaibai/cardfantasy/test/CounterAttackTest.java
+++ workspace/CardFantasyCore/src/cfvbaibai/cardfantasy/test/CounterAttackTest.java
@@ -72,4 +72,45 @@ public class CounterAttackTest {
         Assert.assertEquals(0, 1730 - c光明之龙.getHP());
         Assert.assertEquals(0, 1400 - c秘银巨石像.getHP());
     }
+    
+    /**
+     * 横扫伤害到的所有带反伤技能的卡都会触发各自的反伤技能，若是盾刺，则总是以攻击者为中心的三张卡牌受到反伤，雷盾结算方法相同
+     */
+    @Test
+    public void test横扫_雷盾() {
+        FeatureTestContext context = FeatureValidationTests.prepare(
+            50, 50, "秘银巨石像", "光明之龙", "圣骑士", "凤凰*3", "雷盾");
+        CardInfo c秘银巨石像 = context.addToField(0, 0);
+        CardInfo c光明之龙 = context.addToField(1, 0);
+        CardInfo c圣骑士 = context.addToField(2, 0);
+        context.addToField(3, 1);
+        context.addToField(4, 1);
+        context.addToField(5, 1);
+        RuneInfo r雷盾 = context.addToRune(0, 1);
+
+        context.startGame();
+
+        r雷盾.activate();
+        context.proceedOneRound();
+
+        Assert.assertEquals(800, 1400 - c秘银巨石像.getHP());
+        Assert.assertEquals(1000, 1730 - c光明之龙.getHP());
+        Assert.assertEquals(800, 1060 - c圣骑士.getHP());
+    }
+    
+    /**
+     * 横扫的溅射对象也会发动反伤技能
+     */
+    @Test
+    public void test横扫_反击() {
+        FeatureTestContext context = FeatureValidationTests.prepare(50, 50, "光明之龙", "秘银巨石像", "皇家雄狮");
+        CardInfo c光明之龙 = context.addToField(0, 0);
+        context.addToField(1, 1);
+        context.addToField(2, 1);
+        context.startGame();
+
+        context.proceedOneRound();
+
+        Assert.assertEquals(100, 1730 - c光明之龙.getHP());
+    }
 }
