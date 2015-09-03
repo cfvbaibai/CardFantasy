38f1f5c9af9f6890726d2ad92abc036bd61de390
Tony Nie <tonywhitewhite@hotmail.com>
2015-09-03 16:56:54 +0800

    Add missing image files and POM file

---
+++ workspace/CardFantasyCore/src/cfvbaibai/cardfantasy/test/func/FuncTests.java
@@ -0,0 +1,16 @@
+package cfvbaibai.cardfantasy.test.func;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AttackBuffTest.class, CounterAttackTest.class,
+		DeathSkillTest.class, DefenseTest.class, DelayTest.class,
+		HealingTest.class, MultiAttackTest.class, RacialBufferTest.class,
+		ReviveTest.class, RuneActivationTest.class, SkillSequenceTest.class,
+		SnipeTest.class, SpecialStatusTest.class, SpecificCardTest.class,
+		SpecificRuneTest.class, SummonSkillTest.class, SummonTest.class })
+public class FuncTests {
+
+}
