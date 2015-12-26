c171f385a23e33a964544618325065f8cb608ff8
砍掉重练 <yezhihui_bruce@Hotmail.com>
2015-12-26 12:16:44 +0800

    神祈无法解除沉默,修正此BUG

--- workspace/CardFantasyCore/src/cfvbaibai/cardfantasy/engine/skill/Purify.java
+++ workspace/CardFantasyCore/src/cfvbaibai/cardfantasy/engine/skill/Purify.java
@@ -38,13 +38,14 @@ public final class Purify {
             resolver.removeStatus(card, CardStatusType.裂伤);
             resolver.removeStatus(card, CardStatusType.死印);
             resolver.removeStatus(card, CardStatusType.致盲);
-            resolver.removeStatus(card, CardStatusType.沉默);
+
             // 以下为实测表明净化不能解除的状态
             // resolver.removeStatus(card, CardStatusType.王国);
             // resolver.removeStatus(card, CardStatusType.森林);
             // resolver.removeStatus(card, CardStatusType.蛮荒);
             // resolver.removeStatus(card, CardStatusType.地狱);
             // resolver.removeStatus(card, CardStatusType.魔印);
+            // resolver.removeStatus(card, CardStatusType.沉默);
         }
     }
 }
