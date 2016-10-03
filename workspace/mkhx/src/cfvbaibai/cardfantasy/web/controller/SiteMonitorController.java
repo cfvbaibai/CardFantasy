c3b88d3d97bc0581b8bf36eceadcad1aece41563
Tony Nie <tonywhitewhite@hotmail.com>
2016-10-03 15:22:52 +0800

    Improve monitor table style

--- workspace/mkhx/src/cfvbaibai/cardfantasy/web/controller/SiteMonitorController.java
+++ workspace/mkhx/src/cfvbaibai/cardfantasy/web/controller/SiteMonitorController.java
@@ -31,17 +31,18 @@ public class SiteMonitorController {
             writer.println("<!DOCTYPE html>");
             writer.println("<html>");
             writer.println("<head>");
-            writer.println("<style>td { font-size: 7pt; font-family: 微软雅黑, Calibri, Consolas, Tahoma, Arial; }</style>");
             writer.println("<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>");
             writer.println("<meta name='viewport' content='width=device-width, initial-scale=1'>");
+            writer.println("<style>body { font-size: 9px; font-family: \"Helvetica Neue\",Helvetica,\"Hiragino Sans GB\",\"Microsoft YaHei\",Arial,sans-serif; }");
+            writer.println("table { border-collapse: collapse; } table td { border: solid 1px #006699; padding: 2px; }</style>");
             writer.println("</head>");
             writer.println("<body>");
-            writer.println("<table width='100%' border='0' cellspacing='1' bgcolor='#AAAAFF'>");
-            writer.println("<tr bgcolor='blue' style='color: white; font-weight: bold; text-align: center'>");
+            writer.println("<table width='100%'>");
+            writer.println("<tr style='color: #cccccc; background-color: #003366; font-weight: bold'>");
             writer.println("<td>TimeStamp</td><td>User</td><td>Action</td><td>Content</td></tr>");
             
             for (UserAction action : userActionRecorder.getAllActions()) {
-                String line = String.format("<tr bgcolor='white'><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
+                String line = String.format("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
                         Utils.toString(action.getTimeStamp()), action.getUserId(), action.getActionType(), action.getActionContent());
                 writer.print(line);
             }
