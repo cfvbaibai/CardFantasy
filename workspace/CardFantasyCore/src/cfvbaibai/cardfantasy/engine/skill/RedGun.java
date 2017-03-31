13356c5e231c12275b6329aeec5393ec44c9f147
zqyyou <diemengqiannian@126.com>
2017-03-31 23:17:15 +0800

    鬼才，鹰眼，鬼彻，火攻，毒杀，反击屏障，雷霆一击，觉醒狼顾，赤之魔枪，镜魔，空城，仙子召唤，武侯。 修复涅槃，寒冰触碰。 仲达、仲谋、卧龙、雪舞、黑羽镜姬、诸葛更新完毕。

---
+++ workspace/CardFantasyCore/src/cfvbaibai/cardfantasy/engine/skill/RedGun.java
@@ -0,0 +1,36 @@
+package cfvbaibai.cardfantasy.engine.skill;
+
+import cfvbaibai.cardfantasy.GameUI;
+import cfvbaibai.cardfantasy.data.Skill;
+import cfvbaibai.cardfantasy.data.SkillType;
+import cfvbaibai.cardfantasy.engine.*;
+
+import java.util.List;
+
+/**
+ * Created by hasee on 2017/3/30.
+ */
+public class RedGun {
+    public static void apply(SkillUseInfo skillUseInfo, SkillResolver resolver, EntityInfo attacker, Player defender,
+                             int victimCount) throws HeroDieSignal {
+        List<CardInfo> victims = resolver.getStage().getRandomizer().pickRandom(defender.getField().toList(),
+                victimCount, true, null);
+        GameUI ui = resolver.getStage().getUI();
+        Skill skill = skillUseInfo.getSkill();
+        ui.useSkill(attacker, victims, skill, true);
+        int damage = skill.getImpact();
+        int magnifier = skill.getImpact2();
+        for (CardInfo victim : victims) {
+            if (!resolver.resolveAttackBlockingSkills(attacker, victim, skillUseInfo.getSkill(), damage).isAttackable()) {
+                continue;
+            }
+            int actualDamage = damage;
+            if (victim.containsUsableSkill(SkillType.免疫) || CounterMagic.getBlockSkill(victim) != null) {
+                actualDamage *= magnifier;
+            }
+            ui.attackCard(attacker, victim, skill, actualDamage);
+            resolver.resolveDeathSkills(attacker, victim, skill,
+                    resolver.applyDamage(attacker, victim, skill, actualDamage));
+        }
+    }
+}
