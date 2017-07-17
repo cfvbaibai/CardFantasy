6c3b8d691c04909f087e82cdfd2fc3ffd14e08e8
zqyyou <diemengqiannian@126.com>
2017-07-17 21:17:09 +0800

    更新卡牌【悠风奏者】,【三国英魂·玄德】，修复【玫瑰甜心】二维错误。

---
+++ workspace/CardFantasyCore/src/cfvbaibai/cardfantasy/engine/skill/Horn.java
@@ -0,0 +1,32 @@
+package cfvbaibai.cardfantasy.engine.skill;
+
+import cfvbaibai.cardfantasy.engine.CardInfo;
+import cfvbaibai.cardfantasy.engine.CardStatusItem;
+import cfvbaibai.cardfantasy.engine.SkillUseInfo;
+import cfvbaibai.cardfantasy.engine.SkillResolver;
+import cfvbaibai.cardfantasy.engine.HeroDieSignal;
+import java.util.List;
+
+public final class Horn {
+    public static void apply(SkillUseInfo skillUseInfo, SkillResolver resolver, CardInfo attacker) throws HeroDieSignal {
+        List<CardInfo> allHandCards = attacker.getOwner().getHand().toList();
+        CardInfo target = null;
+        for (CardInfo card : allHandCards) {
+            if (target == attacker) {
+                continue;
+            }
+            if (target == null || card.getSummonDelay() > target.getSummonDelay()) {
+                target = card;
+            }
+        }
+        if (target == null) {
+            // No card in Hand.
+            return;
+        }
+        resolver.getStage().getUI().useSkill(attacker, target, skillUseInfo.getSkill(), true);
+        resolver.summonCard(attacker.getOwner(), target, null, false, skillUseInfo.getSkill());
+        CardStatusItem item = CardStatusItem.weak(skillUseInfo);
+        target.addStatus(item);
+
+    }
+}
